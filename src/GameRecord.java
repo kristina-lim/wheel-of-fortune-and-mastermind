188b6eab76aa8d954ad56c0b8ee8d13d88448909
kristina-lim <kristinahlim@gmail.com>
2024-11-01 15:24:26 -0700

    Add GameRecord class

---
+++ src/GameRecord.java
@@ -0,0 +1,34 @@
+public class GameRecord implements Comparable<GameRecord> {
+    // Variable score keeps track of the score
+    private int score;
+    private String playerId;
+
+    // Constructor
+    public GameRecord(int score, String playerId) {
+        this.score = score;
+        this.playerId = playerId;
+    }
+
+    // Getters for score
+    public int getScore() {
+        return score;
+    }
+
+    // Getters for player ID
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    // Default compareTo method
+    // Compares GameRecord to another GameRecord for sorting purposes
+    @Override
+    public int compareTo(GameRecord other) {
+        return Integer.compare(this.score, other.score);
+    }
+
+    // toString method to show player ID and score
+    @Override
+    public String toString() {
+        return "Player ID: " + playerId + ", Score: " + score;
+    }
+}
