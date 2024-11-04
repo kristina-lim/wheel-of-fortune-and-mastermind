e8b87601331763a5c9a1ab4c85145cd866a960be
kristina-lim <kristinahlim@gmail.com>
2024-11-03 16:18:50 -0800

    Make changes

--- src/WheelOfFortune.java
+++ src/WheelOfFortune.java
@@ -3,7 +3,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 // Abstract class for the Wheel of Fortune game, extends from Game class
 public abstract class WheelOfFortune extends Game {
@@ -13,7 +12,6 @@ public abstract class WheelOfFortune extends Game {
     protected int missedCount;
     protected int maxGuessCount;
     protected List<String> phraseList;
-    private int totalScore;
 
     // Constructor
     public WheelOfFortune(int maxGuessCount) {
@@ -22,7 +20,6 @@ public abstract class WheelOfFortune extends Game {
         this.previousGuesses = "";
         this.missedCount = 0;
         this.maxGuessCount = maxGuessCount;
-        this.totalScore = 0;
         readPhrases();
     }
 
@@ -87,63 +84,4 @@ public abstract class WheelOfFortune extends Game {
 
     // Abstract method to get a guess from the player
     public abstract char getGuess(String previousGuesses);
-
-    // Main game loop
-    @Override
-    public GameRecord play() {
-        // Reset previous guesses for each new game
-        this.previousGuesses = "";
-        this.missedCount = 0;
-
-        System.out.println("Welcome to Wheel of Fortune! The rules are quite simple: ");
-        System.out.println("1) You will have a maximum of " + maxGuessCount + " guesses to figure out the hidden phrase.");
-        System.out.println("2) Only guess one letter at a time.");
-        System.out.println("3) Have fun and good luck!\n");
-        // Select a random phrase
-        randomPhrase();
-        int score = 0;
-
-        // Continuous loop for the guessing process
-        while (true) {
-            System.out.println("Current Phrase: " + hiddenPhrase);
-            System.out.print("Previous guesses: " + previousGuesses + "\n");
-            // Get user guess
-            char guessedLetter = getGuess(previousGuesses);
-            // Process guess
-            boolean correctGuess = processGuess(guessedLetter);
-
-            // Check for game win condition
-            if (hiddenPhrase.indexOf("*") == -1) {
-                score = Math.max(0, maxGuessCount - missedCount);
-                totalScore += score;
-                System.out.println("Congratulations!");
-                System.out.println("You guessed the phrase: " + this.phrase + ".");
-                System.out.println("You've won with a score of: " + score);
-                System.out.println("Your total score is now: " + totalScore);
-
-                // Discard the used phrase
-                phraseList.remove(phrase);
-                break;
-            }
-
-            // Check for game over condition
-            if (missedCount >= maxGuessCount) {
-                System.out.println("Game over! The phrase was: " + phrase);
-                break;
-            }
-        }
-
-        return new GameRecord(score, "Player ID"); // Handle player ID logic as needed
-    }
-
-    // Method to ask if the user wants to play again
-    @Override
-    public boolean playNext() {
-        // If no phrases left, no need to ask
-        if (phraseList.isEmpty()) {
-            return false;
-        }
-            return true; // Default implementation allows playing next
-    }
-
 }
--- src/WheelOfFortuneUserGame.java
+++ src/WheelOfFortuneUserGame.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 // Allows the user to play each game with a random phrase, and if there are more phrases, ask after the game if the player wants to continue
 public class WheelOfFortuneUserGame extends WheelOfFortune {
+    private int totalScore;
+
     // Constructor
     public WheelOfFortuneUserGame(int maxGuessCount) {
         super(maxGuessCount);
+        this.totalScore = 0;
     }
 
     // Method to get a valid letter guess from user
@@ -46,6 +49,55 @@ public class WheelOfFortuneUserGame extends WheelOfFortune {
             break;
         }
         return guessedLetter;
+    }
+
+    // Main game loop
+    @Override
+    public GameRecord play() {
+        // Reset previous guesses for each new game
+        this.previousGuesses = "";
+        this.missedCount = 0;
+
+        System.out.println("Welcome to Wheel of Fortune! The rules are quite simple: ");
+        System.out.println("1) You will have a maximum of " + maxGuessCount + " guesses to figure out the hidden phrase.");
+        System.out.println("2) Only guess one letter at a time.");
+        System.out.println("3) Have fun and good luck!\n");
+
+        // Select a random phrase
+        randomPhrase();
+        int score = 0;
+
+        // Continuous loop for the guessing process
+        while (true) {
+            System.out.println("Current Phrase: " + hiddenPhrase);
+            System.out.print("Previous guesses: " + previousGuesses + "\n");
+            // Get user guess
+            char guessedLetter = getGuess(previousGuesses);
+            // Process guess
+            boolean correctGuess = processGuess(guessedLetter);
+
+            // Check for game win condition
+            if (hiddenPhrase.indexOf("*") == -1) {
+                score = Math.max(0, maxGuessCount - missedCount);
+                totalScore += score;
+                System.out.println("Congratulations!");
+                System.out.println("You guessed the phrase: " + this.phrase + ".");
+                System.out.println("You've won with a score of: " + score);
+                System.out.println("Your total score is now: " + totalScore);
+
+                // Discard the used phrase
+                phraseList.remove(phrase);
+                break;
+            }
+
+            // Check for game over condition
+            if (missedCount >= maxGuessCount) {
+                System.out.println("Game over! The phrase was: " + phrase);
+                break;
+            }
+        }
+
+        return new GameRecord(totalScore, "Player ID"); // Handle player ID logic as needed
     }
 
     // Method to ask if the user wants to play again
